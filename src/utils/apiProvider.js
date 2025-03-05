a1e22197aab79562d7997e687ca2c82961b8b244
MANOJ GORE <manojgore.mg@gmail.com>
2025-03-05 22:49:25 +0530

    kjklj

--- src/utils/apiProvider.js
+++ src/utils/apiProvider.js
@@ -1,6 +1,6 @@
-export const api = "http://localhost:3001";
-// export const api = "https://app.jiokheti.com"  //backend url
-export const webSocketServer ='ws://localhost:3001';
-// export const webSocketServer = "wss://app.jiokheti.com"   //backend url with 8080 port
-export const krushiMahaDomain = "http://localhost:3000";
-// export const krushiMahaDomain = "http://jiokheti.com";
+// export const api = "http://localhost:3001";
+export const api = "https://app.jiokheti.com"  //backend url
+// export const webSocketServer ='ws://localhost:3001';
+export const webSocketServer = "wss://app.jiokheti.com"   //backend url with 8080 port
+// export const krushiMahaDomain = "http://localhost:3000";
+export const krushiMahaDomain = "https://www.jiokheti.com";
