2bb01565aee9427bfa02a7fdc48c9fb5956565dd
MANOJ GORE <manojgore.mg@gmail.com>
2025-03-08 09:07:14 +0530

    changes

--- src/utils/apiProvider.js
+++ src/utils/apiProvider.js
@@ -3,4 +3,4 @@ export const api = "http://localhost:3001";
 export const webSocketServer ='ws://localhost:3001';
 // export const webSocketServer = "wss://app.jiokheti.com"   //backend url with 8080 port
 export const krushiMahaDomain = "http://localhost:3000";
-// export const krushiMahaDomain = "https://www.jiokheti.com";
+// export const krushiMahaDomain = "https://www.jiokheti.com"; 
