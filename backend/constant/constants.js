954ccca6cb31c24c3a4355faff144f647add3c59
pchavan <pavan.chavan@forescout.com>
2025-03-12 19:58:04 +0530

    added local url

--- backend/constant/constants.js
+++ backend/constant/constants.js
@@ -2,7 +2,8 @@ const constants = {
 	allowedOrigins : [
 		process.env.ADMIN_FRONTEND_DOMAIN,
 		process.env.KRUSHI_MAHA_FRONTEND_DOMAIN,
-		process.env.ADMIN_FRONTEND_LOCAL_URL
+		process.env.ADMIN_FRONTEND_LOCAL_URL,
+		process.env.LOCAL_JIO_KHETI_FRONTEND_DOMAIN
 	]
 }
 
