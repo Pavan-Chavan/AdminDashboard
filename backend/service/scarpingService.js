70aad960afbc72a1b6fc94208c11015107926061
pchavan <pavan.chavan@forescout.com>
2025-03-26 17:40:01 +0530

    bajarbhav changes

--- backend/service/scarpingService.js
+++ backend/service/scarpingService.js
@@ -19,41 +19,40 @@ const scarpingWeb = async (marketTypes, ws, marketTypesDetails) => {
       const marketTypeData = marketTypesDetails[marketType];
 
       // Iterate over each dropdown option sequentially
-      const processBatch = async (batch) => {
-        await Promise.all(batch.map(async (option) => {
-          try {
+      for (const option of marketTypeData.DropdownOptions) {
+      try {
         ws.send(JSON.stringify({
-          status: "info",
-          message: `Launching browser for Market Type: ${marketType}, Name: ${option.name}, Code: ${option.code}`
+        status: "info",
+        message: `Launching browser for Market Type: ${marketType}, Name: ${option.name}, Code: ${option.code}`
         }));
 
         const browser = await puppeteer.launch();
 
         ws.send(JSON.stringify({
-          status: "info",
-          message: `Launching web page for Market Type: ${marketType}, Name: ${option.name}, Code: ${option.code}`
+        status: "info",
+        message: `Launching web page for Market Type: ${marketType}, Name: ${option.name}, Code: ${option.code}`
         }));
 
         const page = await browser.newPage();
         const URL = `${baseURL}#${marketType}`;
 
         ws.send(JSON.stringify({
-          status: "info",
-          message: `Loading into ${URL} for Market Type: ${marketType}, Name: ${option.name}, Code: ${option.code}`
+        status: "info",
+        message: `Loading into ${URL} for Market Type: ${marketType}, Name: ${option.name}, Code: ${option.code}`
         }));
 
         await page.goto(URL, { waitUntil: 'domcontentloaded' });
 
         // Update language
         const changingLanguage = await page.evaluate(() => {
-          const selectElement = document.getElementById('language');
-          if (selectElement) {
-            selectElement.value = 'मराठी';
-            const event = new Event('change', { bubbles: true });
-            selectElement.dispatchEvent(event);
-            return `Updated language to मराठी`;
-          }
-          return `Did not find language dropdown`;
+        const selectElement = document.getElementById('language');
+        if (selectElement) {
+          selectElement.value = 'मराठी';
+          const event = new Event('change', { bubbles: true });
+          selectElement.dispatchEvent(event);
+          return `Updated language to मराठी`;
+        }
+        return `Did not find language dropdown`;
         });
         ws.send(JSON.stringify({ status: "info", message: changingLanguage }));
 
@@ -61,12 +60,12 @@ const scarpingWeb = async (marketTypes, ws, marketTypesDetails) => {
 
         // Switch tab
         const switchingTab = await page.evaluate((marketTypeData, option) => {
-          const selectElement = document.getElementById(marketTypeData.subTabID);
-          if (selectElement) {
-            selectElement.click();
-            return `Switching tab for ${marketTypeData.subTabID} Market Type: ${marketTypeData.name}, Name: ${option.name}, Code: ${option.code}`;
-          }
-          return `ID not found for ${marketTypeData.subTabID}`;
+        const selectElement = document.getElementById(marketTypeData.subTabID);
+        if (selectElement) {
+          selectElement.click();
+          return `Switching tab for ${marketTypeData.subTabID} Market Type: ${marketTypeData.name}, Name: ${option.name}, Code: ${option.code}`;
+        }
+        return `ID not found for ${marketTypeData.subTabID}`;
         }, marketTypeData, option);
 
         ws.send(JSON.stringify({ status: "info", message: switchingTab }));
@@ -74,21 +73,21 @@ const scarpingWeb = async (marketTypes, ws, marketTypesDetails) => {
 
         // Manipulate dropdown
         const manipulationResult = await page.evaluate((marketTypeData, option) => {
-          const selectElement = document.getElementById(marketTypeData.SelectOptionId);
-          if (selectElement) {
-            const optionsList = [...selectElement.options].map(opt => opt.value);
-            if (optionsList.includes(option.code)) {
+        const selectElement = document.getElementById(marketTypeData.SelectOptionId);
+        if (selectElement) {
+          const optionsList = [...selectElement.options].map(opt => opt.value);
+          if (optionsList.includes(option.code)) {
           selectElement.value = option.code;
-            } else {
+          } else {
           const string = optionsList.map(opt => `'${opt}'`).join(', ');
           return `Invalid option code: ${option.code}. Available options: ${string}`;
-            }
-            const valueOFSelect = selectElement.value;
-            const event = new Event('change', { bubbles: true });
-            selectElement.dispatchEvent(event);
-            return `Dropdown value updated and onchange triggered for valueOFSelect: ${valueOFSelect} Market Type: ${marketTypeData.name}, Name: ${option.name}, Code: ${option.code}, option: ${marketTypeData.SelectOptionId}`;
           }
-          return `Dropdown not found for ${marketTypeData.name}`;
+          const valueOFSelect = selectElement.value;
+          const event = new Event('change', { bubbles: true });
+          selectElement.dispatchEvent(event);
+          return `Dropdown value updated and onchange triggered for valueOFSelect: ${valueOFSelect} Market Type: ${marketTypeData.name}, Name: ${option.name}, Code: ${option.code}, option: ${marketTypeData.SelectOptionId}`;
+        }
+        return `Dropdown not found for ${marketTypeData.name}`;
         }, marketTypeData, option);
 
         ws.send(JSON.stringify({ status: "info", message: manipulationResult }));
@@ -97,36 +96,36 @@ const scarpingWeb = async (marketTypes, ws, marketTypesDetails) => {
 
         // Scrape data
         const data = await page.evaluate((marketTypeData) => {
-          const items = document.getElementById(marketTypeData.tableId);
-          return items ? items.outerHTML : null;
+        const items = document.getElementById(marketTypeData.tableId);
+        return items ? items.outerHTML : null;
         }, marketTypeData);
 
         if (!data) {
-          ws.send(JSON.stringify({
-            status: "error",
-            message: `No data found in ${marketTypeData.name} - ${option.name}`
-          }));
+        ws.send(JSON.stringify({
+          status: "error",
+          message: `No data found in ${marketTypeData.name} - ${option.name}`
+        }));
         } else {
-          // Prepare data for API call
-          const payload = {
-            tableName: marketTypeData.tableName,
-            table_data: JSON.stringify(data),
-            code: option.code,
-            slug: option.slug
-          };
+        // Prepare data for API call
+        const payload = {
+          tableName: marketTypeData.tableName,
+          table_data: JSON.stringify(data),
+          code: option.code,
+          slug: option.slug
+        };
 
-          // Call the endpoint
-          try {
-            const response = await axios.post(
+        // Call the endpoint
+        try {
+          const response = await axios.post(
           `${process.env.ADMIN_BACKEND_DOMAIN}/api/insert-market-data`,
           payload,
           {
             httpsAgent: new https.Agent({
-              rejectUnauthorized: false // Bypasses SSL verification
+            rejectUnauthorized: false // Bypasses SSL verification
             })
           }
-            );
-            ws.send(JSON.stringify({
+          );
+          ws.send(JSON.stringify({
           status: response.data.status,
           message: response.data.message,
           data: {
@@ -135,10 +134,10 @@ const scarpingWeb = async (marketTypes, ws, marketTypesDetails) => {
             Code: option.code,
             insertId: response.data.insertId
           }
-            }));
-          } catch (apiError) {
-            console.log(apiError);
-            ws.send(JSON.stringify({
+          }));
+        } catch (apiError) {
+          console.log(apiError);
+          ws.send(JSON.stringify({
           status: "error",
           message: `API call failed: ${apiError.message}`,
           data: {
@@ -146,41 +145,34 @@ const scarpingWeb = async (marketTypes, ws, marketTypesDetails) => {
             Name: option.name,
             Code: option.code
           }
-            }));
-          }
+          }));
+        }
         }
 
         await browser.close();
 
-          } catch (error) {
+      } catch (error) {
         ws.send(JSON.stringify({
-          status: "error",
-          message: `Error processing ${marketType} - ${option.name}: ${error.message}`,
-          data: {
-            Section: marketType,
-            Name: option.name,
-            Code: option.code
-          }
+        status: "error",
+        message: `Error processing ${marketType} - ${option.name}: ${error.message}`,
+        data: {
+          Section: marketType,
+          Name: option.name,
+          Code: option.code
+        }
         }));
         console.error(`Error processing ${marketType} - ${option.name}:`, error);
-          } finally {
+      } finally {
         // Update progress after each option is processed
         processedOptions++;
         const percentageCompleted = (processedOptions / totalOptions) * 100;
 
         ws.send(JSON.stringify({
-          status: "progress",
-          percentage: percentageCompleted.toFixed(2),
-          message: `Processed ${processedOptions} of ${totalOptions} options`
+        status: "progress",
+        percentage: percentageCompleted.toFixed(2),
+        message: `Processed ${processedOptions} of ${totalOptions} options`
         }));
-          }
-        }));
-      };
-
-      const batchSize = process.env.BAJARBHAV_BATCH_SIZE; // Define the batch size
-      for (let i = 0; i < marketTypeData.DropdownOptions.length; i += batchSize) {
-        const batch = marketTypeData.DropdownOptions.slice(i, i + batchSize);
-        await processBatch(batch);
+      }
       }
     }
 
--- src/pages/bajarbhav-pulling/WebSocketComponent.jsx
+++ src/pages/bajarbhav-pulling/WebSocketComponent.jsx
@@ -39,7 +39,7 @@ const WebSocketComponent = () => {
             setStatusUpdates((prev) => [...prev, `${data.message}`]);
             switch (data.status) {
               case "error":
-                setFailed((prev)=>([...prev,data.data]))
+                setFailed((prev)=>([...prev,data]))
                 break;
               case "insert":
                 setInsert((prev)=>([...prev,data.data]))
@@ -245,7 +245,7 @@ const WebSocketComponent = () => {
                 </div>
                 <div className="container mt-5">
                   <h3 className="mb-4">Error data</h3>
-                    {failed.map((data)=>(<div  className='text-12 lh-16 fw-500 uppercase bg-dark-1 text-white tag error' style={{width: 'fit-content'}}> Section : {data?.Section || ""} Name : {data?.Name || ""} Code : {data?.Code || ""}</div>))}
+                    {failed.map((data)=>(<div  className='text-12 lh-16 fw-500 uppercase bg-dark-1 text-white tag error' style={{width: 'fit-content'}}> Section : {data?.data?.Section || ""} Name : {data?.data?.Name || ""} Code : {data?.data?.Code || ""} Error : {data.message}</div>))}
                 </div>
               </div>
             </div>
