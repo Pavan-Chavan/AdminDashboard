d51594811f3e805287f7e715254b49db6a2e2017
pchavan <pavan.chavan@forescout.com>
2025-04-01 08:48:34 +0530

    allow domain

--- backend/Utils/utils.js
+++ backend/Utils/utils.js
@@ -1,7 +1,11 @@
 const constants = require("../constant/constants");
 
 const checkIfOriginAllow = (origin, callback) => {
-	if (!origin || constants.allowedOrigins.includes(origin)) {
+	if (!origin || constants.allowedOrigins.some(allowedOrigin => {
+		const originDomain = new URL(origin).hostname.replace(/^www\./, '');
+		const allowedDomain = new URL(allowedOrigin).hostname.replace(/^www\./, '');
+		return originDomain === allowedDomain;
+	})) {
 		callback(null, true);
 	} else {
 		callback(new Error("Not allowed by CORS"));
